2baf4d53d01cf9dfe648953268092ade4f9292cf
Yannick HEINRICH <yannick.heinrich@gmail.com>
2011-12-04 01:00:15 +0100

    Initial Commit

---
+++ GnuplotObjc/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  GnuplotObjc
+//
+//  Created by HEINRICH Yannick on 04/12/11.
+//  Copyright (c) 2011 UNICAEN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
